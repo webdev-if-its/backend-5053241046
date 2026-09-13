7ee70b28946151791a48a215cf2ce78c3ab1ee50
RootKanya <berlianyafikaniamuawanah@gmail.com>
2026-09-13 19:21:57 +0700

    level 9 selesai

--- pertemuan-02/main.go
+++ pertemuan-02/main.go
@@ -104,7 +104,11 @@ func RingkasanPesanan(qty []int, hargaSatuan []float64, tarifPajak float64) stri
 // TODO(Level 9): signature ini SUDAH benar (cari tahu sendiri kenapa
 // bentuknya begini - lihat SOAL.md) - tinggal implementasikan isinya.
 func Total(harga ...float64) float64 {
-	panic("belum diimplementasikan")
+	var total float64
+	for _, h := range harga {
+		total += h
+	}
+	return total
 }
 
 // TODO(Level 10, bonus): signature ini SUDAH benar (cari tahu sendiri
